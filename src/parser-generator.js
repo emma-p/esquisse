1efe892254c09611c2b4624f0cd823cf886aba86
Emmanuelle Poirier <manuepoirier@gmail.com>
2014-12-06 20:36:26 +0000

    Put grammar inline

--- src/parser-generator.js
+++ src/parser-generator.js
@@ -1,11 +1,20 @@
 'use strict';
 
 var PEG = require("pegjs");
-var fs = require("fs");
-var path = require("path");
 
-var grammarPath = path.join(__dirname, "grammar.pegjs");
-var grammar = fs.readFileSync(grammarPath, { encoding: 'utf8' });
+var grammar = [
+  'start = docTitle "\\n" node+',
+  'docTitle = doc:("desktop") " " title:title { return { doctype: doc, title: title } }',
+  'title = t:[a-z]i+ { return t.join("") }',
+  'node = indent:indent keyword:keyword " " "[" attributes:attributes+ "]" delimiter internalNode:internalNode* { return { keyword: keyword, attributes: attributes, children: internalNode } }',
+  'indent = "  "',
+  'keyword = "navbar" / "button"',
+  'attr = "size" / "offset"',
+  'digit = [0-9a-z]i',
+  'delimiter = !.  / "\\n"',
+  'attributes = a:attr ":" v:digit+ " "* { return { attr: a, value: parseInt(v.join(""),10) } }',
+  'internalNode = indent node'
+  ].join("\n");
 
 function init() {
   return PEG.buildParser(grammar);
